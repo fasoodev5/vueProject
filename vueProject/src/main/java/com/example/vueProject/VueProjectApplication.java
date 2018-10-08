44d247e86d243c21a559d24bfd802deffadb3490
dnfkr <dnfkr@DESKTOP-I30CD38>
2018-10-08 14:32:27 +0900

    spring boot skeleton

---
+++ vueProject/src/main/java/com/example/vueProject/VueProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.vueProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueProjectApplication.class, args);
+	}
+}
---
+++ vueProject/src/main/java/com/example/vueProject/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.vueProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "Test Page";
+	}
+}
