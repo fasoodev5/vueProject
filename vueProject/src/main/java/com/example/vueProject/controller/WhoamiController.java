a326f251233757b43810cd71ff5ca6c47a24a01a
chuhwasung <chuhwasung@gmail.com>
2018-10-08 16:12:28 +0900

    Commit Test

--- vueProject/src/main/java/com/example/vueProject/controller/WhoamiController.java
+++ vueProject/src/main/java/com/example/vueProject/controller/WhoamiController.java
@@ -3,6 +3,6 @@ package com.example.vueProject.controller;
 public class WhoamiController {
 	private void Test() {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		System.out.println("Test Change Test");
 	}
 }
