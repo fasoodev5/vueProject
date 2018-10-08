d1fe7ec1b15e6d9d49f5c3f02b1a4063e6a52f4a
dnfkr <dnfkr@DESKTOP-I30CD38>
2018-10-08 14:49:24 +0900

    Jack commit

--- vueProject/src/main/java/com/example/vueProject/controller/HomeController.java
+++ vueProject/src/main/java/com/example/vueProject/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
-		return "Test Page commit Test";
+		return "Test Page commit Test Jack";
 	}
 }
